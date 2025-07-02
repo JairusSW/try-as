5d41903634a592cb6dc6b01411855be0d14e1c19
JairusSW <me@jairus.dev>
2025-07-02 10:47:22 -0700

    feat: add .clone method

--- assembly/types/exception.ts
+++ assembly/types/exception.ts
@@ -15,71 +15,80 @@ export namespace ExceptionState {
 }
 
 export class Exception {
-  // @ts-ignore
-  private __IS_EXCEPTION_TYPE: boolean = false;
   public type: ExceptionType;
+
   // Abort
-  public get msg(): string | null {
-    return this.type == ExceptionType.Abort ? this.msg : null;
-  }
-  public get fileName(): string | null {
-    return this.type == ExceptionType.Abort ? this.fileName : null;
-  }
-  public get lineNumber(): i32 {
-    return this.type == ExceptionType.Abort ? this.lineNumber : -1;
-  }
-  public get columnNumber(): i32 {
-    return this.type == ExceptionType.Abort ? this.columnNumber : -1;
-  }
+  public msg: string | null = null;
+  public fileName: string | null = null;
+  public lineNumber: i32 = -1;
+  public columnNumber: i32 = -1;
 
   // Error
-  public get message(): string | null {
-    return this.type == ExceptionType.Throw && (ErrorState.isErrorType || ErrorState.hasMessage) ? this.message : null;
-  }
-  public get name(): string | null {
-    return this.type == ExceptionType.Throw && ErrorState.isErrorType ? this.name : null;
-  }
-  public get stack(): string | null {
-    return this.type == ExceptionType.Throw && ErrorState.isErrorType ? this.stack : null;
-  }
+  public message: string | null = null;
+  public name: string | null = null;
+  public stack: string | null = null;
 
   constructor(type: ExceptionType) {
     this.type = type;
+
+    if (type == ExceptionType.Abort) {
+      this.msg = AbortState.msg;
+      this.fileName = AbortState.fileName;
+      this.lineNumber = AbortState.lineNumber;
+      this.columnNumber = AbortState.columnNumber;
+    } else if (type == ExceptionType.Throw) {
+      if (ErrorState.isErrorType || ErrorState.hasMessage) {
+        this.message = ErrorState.message;
+      }
+      if (ErrorState.isErrorType) {
+        this.name = ErrorState.name;
+        this.stack = ErrorState.stack;
+      }
+      this.fileName = ErrorState.fileName;
+      this.lineNumber = ErrorState.lineNumber;
+      this.columnNumber = ErrorState.columnNumber;
+    }
   }
+
   toString(): string {
     let out = "";
     if (this.type == ExceptionType.Abort) {
       out = "abort";
-      if (AbortState.msg) out += ": " + AbortState.msg!;
-      if (AbortState.fileName) out += " in " + AbortState.fileName!;
-      if (AbortState.lineNumber) out += ` in (${AbortState.lineNumber}:${AbortState.columnNumber})`;
+      if (this.msg) out += ": " + this.msg!;
+      if (this.fileName) out += " in " + this.fileName!;
+      if (this.lineNumber >= 0 && this.columnNumber >= 0)
+        out += ` in (${this.lineNumber}:${this.columnNumber})`;
     } else if (this.type == ExceptionType.Unreachable) {
       out = "unreachable";
-    } else if (this.type == ExceptionType.Throw && ErrorState.hasMessage) {
-      out = "Error: " + ErrorState.message;
+    } else if (this.type == ExceptionType.Throw && this.message) {
+      out = "Error: " + this.message!;
     }
     return out;
   }
+
   // @ts-ignore: inline
   @inline is<T>(): boolean {
     if (this.type != ExceptionType.Throw) return false;
     return ErrorState.discriminator == DISCRIMINATOR<T>();
   }
+
   // @ts-ignore: inline
   @inline as<T>(): T {
     if (this.type != ExceptionType.Throw) return changetype<T>(0);
     if (ErrorState.discriminator != DISCRIMINATOR<T>()) return changetype<T>(0);
     return load<T>(ErrorState.storage);
   }
+
   rethrow(): void {
     if (this.type == ExceptionType.Abort) {
       abort(this.msg, this.fileName, this.lineNumber, this.columnNumber);
     } else if (this.type == ExceptionType.Unreachable) {
       unreachable();
     } else if (this.type == ExceptionType.Throw) {
-      abort(ErrorState.hasMessage ? ErrorState.message : null, ErrorState.fileName, ErrorState.lineNumber, ErrorState.columnNumber);
+      abort(this.message, this.fileName, this.lineNumber, this.columnNumber);
     }
   }
+
   private __try_rethrow(): void {
     if (this.type == ExceptionType.Abort) {
       AbortState.abort(this.msg, this.fileName, this.lineNumber, this.columnNumber);
@@ -89,5 +98,17 @@ export class Exception {
       ExceptionState.Failures++;
       ExceptionState.Type = ExceptionType.Throw;
     }
+  }
+
+  clone(): Exception {
+    let copy = new Exception(this.type);
+    copy.msg = this.msg;
+    copy.fileName = this.fileName;
+    copy.lineNumber = this.lineNumber;
+    copy.columnNumber = this.columnNumber;
+    copy.message = this.message;
+    copy.name = this.name;
+    copy.stack = this.stack;
+    return copy;
   }
 }
